b694279c3c5f05e14f9d3236561bf5c68cc8b73c
Florian Geigl <florian.geigl@student.tugraz.at>
2015-11-02 17:20:07 +0100

    switched to DeclareRobustCommand

--- comment_cmd_gen.py
+++ comment_cmd_gen.py
@@ -5,12 +5,13 @@ import sys
 # usage: python comment_cmd_gen.py name1 name2 name3
 # copy output into your latex document
 name_tags = sorted(sys.argv[1:])
+print('% comment cmds')
 for name_tag in name_tags:
     print('\\newcounter{' + name_tag + 'counter}')
-    print('\\newcommand{\\' + name_tag + '}[1]{\\textbf{/* #1 (' + name_tag + ') */}'
+    print('\\DeclareRobustCommand{\\' + name_tag + '}[1]{\\textbf{/* #1 (' + name_tag + ') */}'
         '\\stepcounter{' + name_tag + 'counter}'
         '\\typeout{LaTeX Warning: ' + name_tag + ' comment \\the' + name_tag + 'counter}}')
-print('\\iffalse')
+print('% comment toggle. change to \\iftrue to hide comments\n\\iffalse')
 for name_tag in name_tags:
     print('\t\\renewcommand{\\' + name_tag + '}[1]{}')
 print('\\fi')
