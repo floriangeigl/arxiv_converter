966d1fb008aa4a31b2595ea327a22288b52321b7
Florian Geigl <florian.geigl@tugraz.at>
2016-03-15 22:41:30 +0100

    resolve figure cmds

--- arxiv_converter.py
+++ arxiv_converter.py
@@ -5,100 +5,176 @@ import shutil
 import tex_utils
 import traceback
 from optparse import OptionParser
+from collections import Counter, defaultdict
 
 
-def add_content_of_file(input_filename, output_file, output_folder, file_mapping, remove_comments=True):
+def replace_figure(orig_plot_fn, out_folder, file_mapping):
+    possible_filenames = [orig_plot_fn + i for i in ['', '.pdf', '.png', '.eps']]
+    print('plot filename:', end=' ')
+    try:
+        plot_filename = list(filter(os.path.isfile, possible_filenames))[0]
+        print(plot_filename, '->', end=' ')
+    except IndexError:
+        print(orig_plot_fn, '[FILE NOT FOUND]')
+        return None, None
+    dest_filename = out_folder + os.path.basename(plot_filename)
+    dest_ext = '.' + dest_filename.rsplit('.', 1)[0]
+    try:
+        dest_filename = file_mapping[plot_filename]
+    except KeyError:
+        tmp_filename = dest_filename
+        counter = 0
+        while os.path.isfile(dest_filename):
+            counter += 1
+            tmp_filename = dest_filename.replace(dest_ext, '') + str(counter).rjust(3,
+                                                                                    '0') + dest_ext
+        dest_filename = tmp_filename
+        file_mapping[plot_filename] = dest_filename
+    print('/'.join(dest_filename.rsplit('/', 2)[-2:]), end=' ')
+    shutil.copy(plot_filename, dest_filename)
+    print('[OK]')
+    return plot_filename, os.path.basename(dest_filename).replace(dest_ext, '')
+
+
+def add_content_of_file(input_filename, output_file, output_folder, file_mapping, var_mapping, complex_cmds,
+                        remove_comments=True):
     if not os.path.isfile(input_filename):
         input_filename += '.tex'
-    with open(input_filename, 'r') as f:
-        for line in f:
-            if not line.strip().startswith('%'):
-                if '%' in line and remove_comments:
-                    cleaned_line = list()
-                    for part in line.split('%'):
-                        cleaned_line.append(part)
-                        if not part.endswith('\\'):
-                            break
-                    line = '%'.join(cleaned_line)
-                    if not line.endswith('\n'):
-                        line += '\n'
+    f = tex_utils.FileIter(input_filename)
+    for line in f.get_line():
+        if r'\newcommand{' not in line and r'\renewcommand{' not in line:
+            for var_name, var_val in sorted(var_mapping.iteritems(), key=lambda x: len(x[0]), reverse=True):
+                if var_name not in complex_cmds:
+                    line = line.replace(var_name, var_val)
+        if not line.strip().startswith('%'):
+            if '%' in line and remove_comments:
+                cleaned_line = list()
+                for part in line.split('%'):
+                    cleaned_line.append(part)
+                    if not part.endswith('\\'):
+                        break
+                line = '%'.join(cleaned_line)
+                line += '%'
+                if not line.endswith('\n'):
+                    line += '\n'
 
-                if r'\input{' in line:
-                    for cmd, import_filename in tex_utils.simple_cmd_match.findall(line):
-                        if cmd == 'input':
-                            if not remove_comments:
-                                output_file.write('%' + '=' * 80 + '\n')
-                                output_file.write('%imported external file: ' + import_filename + '\n')
-                            print('import external file content:', import_filename)
-                            add_content_of_file(import_filename, output_file, output_folder, file_mapping,
-                                                remove_comments=remove_comments)
-                            if not remove_comments:
-                                output_file.write('%end external file: ' + import_filename + '\n')
-                                output_file.write('%' + '=' * 80 + '\n')
-                            print('import external file content:', import_filename, '[DONE]')
-                elif r'\includegraphics' in line:
-                    for used_plot_filename in tex_utils.graphics_cmd_match.findall(line):
-                        possible_filenames = [used_plot_filename + i for i in ['', '.pdf', '.png', '.eps']]
-                        print('plot filename:', end=' ')
-                        try:
-                            plot_filename = list(filter(os.path.isfile, possible_filenames))[0]
-                            print(plot_filename, '->', end=' ')
-                        except IndexError:
-                            print(used_plot_filename, '[FILE NOT FOUND]')
-                            continue
-                        dest_filename = output_folder + os.path.basename(plot_filename)
-                        dest_ext = '.' + dest_filename.rsplit('.', 1)[0]
-                        try:
-                            dest_filename = file_mapping[plot_filename]
-                        except KeyError:
-                            tmp_filename = dest_filename
-                            counter = 0
-                            while os.path.isfile(dest_filename):
-                                counter += 1
-                                tmp_filename = dest_filename.replace(dest_ext, '') + str(counter).rjust(3,
-                                                                                                        '0') + dest_ext
-                            dest_filename = tmp_filename
-                            file_mapping[plot_filename] = dest_filename
-                        print('/'.join(dest_filename.rsplit('/', 2)[-2:]), end=' ')
-                        shutil.copy(plot_filename, dest_filename)
-                        output_file.write(
-                            line.replace(used_plot_filename, os.path.basename(dest_filename).replace(dest_ext, '')))
-                        print('[OK]')
-                elif r'\bibliography{' in line:
-                    compiled_bibtex_file = input_filename.replace('.tex', '.bbl')
-                    if not os.path.isfile(compiled_bibtex_file):
-                        article_file = input_filename.replace('.tex', '')
-                        if not os.path.isfile(input_filename.replace('.tex', '.aux')):
-                            print('try compile paper:', end=' ')
-                            sys.stdout.flush()
-                            if os.system('pdflatex ' + article_file) == 0:
-                                print('[OK]')
+            line_simple_cmd = tex_utils.simple_cmd_match.findall(line.strip())
+            # print(line.strip(), '\n\t->', line_simple_cmd)
 
-                            else:
-                                print('[FAILED]')
+            if r'\input{' in line:
+                for cmd, import_filename in tex_utils.simple_cmd_match.findall(line):
+                    if cmd == 'input':
+                        if not remove_comments:
+                            output_file.write('%' + '=' * 80 + '\n')
+                            output_file.write('%imported external file: ' + import_filename + '\n')
+                        print('import external file content:', import_filename)
+                        add_content_of_file(import_filename, output_file, output_folder, file_mapping, var_mapping,
+                                            complex_cmds, remove_comments=remove_comments)
+                        if not remove_comments:
+                            output_file.write('%end external file: ' + import_filename + '\n')
+                            output_file.write('%' + '=' * 80 + '\n')
+                        print('import external file content:', import_filename, '[DONE]')
+            elif r'\includegraphics' in line:
+                for orig_plot_fn in tex_utils.graphics_cmd_match.findall(line):
+                    orig_fn, dest_fn = replace_figure(orig_plot_fn, output_folder, file_mapping)
+                    if orig_fn is not None:
+                        line = line.replace(orig_plot_fn, dest_fn)
+                output_file.write(line)
+            elif r'\bibliography{' in line:
+                compiled_bibtex_file = input_filename.replace('.tex', '.bbl')
+                if not os.path.isfile(compiled_bibtex_file):
+                    article_file = input_filename.replace('.tex', '')
+                    if not os.path.isfile(input_filename.replace('.tex', '.aux')):
                         print('try compile paper:', end=' ')
                         sys.stdout.flush()
-                        if os.system('bibtex ' + article_file) == 0:
+                        if os.system('pdflatex ' + article_file) == 0:
                             print('[OK]')
+
                         else:
                             print('[FAILED]')
-                    try:
-                        if not remove_comments:
-                            output_file.write('%' + '=' * 80 + '\n')
-                            output_file.write('% bibtex content\n')
-                        with open(compiled_bibtex_file, 'r') as bib_f:
-                            for bib_line in bib_f:
-                                if not bib_line.strip().startswith('%') or not remove_comments:
-                                    output_file.write(bib_line)
-                        if not remove_comments:
-                            output_file.write('\n% end bibtex\n')
-                            output_file.write('%' + '=' * 80 + '\n')
-                    except:
-                        print(traceback.format_exc())
+                    print('try compile paper:', end=' ')
+                    sys.stdout.flush()
+                    if os.system('bibtex ' + article_file) == 0:
+                        print('[OK]')
+                    else:
+                        print('[FAILED]')
+                try:
+                    if not remove_comments:
+                        output_file.write('%' + '=' * 80 + '\n')
+                        output_file.write('% bibtex content\n')
+                    with open(compiled_bibtex_file, 'r') as bib_f:
+                        for bib_line in bib_f:
+                            if not bib_line.strip().startswith('%') or not remove_comments:
+                                output_file.write(bib_line)
+                    if not remove_comments:
+                        output_file.write('\n% end bibtex\n')
+                        output_file.write('%' + '=' * 80 + '\n')
+                except:
+                    print(traceback.format_exc())
+            elif r'\newcommand{' in line or r'\renewcommand{' in line:
+                #print('newcmd line:', line)
+                # \newcommand{\varname}{var_val}
+                braces_counter = defaultdict(int, Counter(line))
+                braces = braces_counter['{'] - braces_counter['}']
+                output_file.write(line)
+                if braces == 0:
+                    for var_name, var_val in tex_utils.newcmd_match.findall(line):
+                        var_mapping[var_name] = var_val
+                        # print('map var:',var_name, var_val)
+                else:
+                    tmp_cmd = [line.strip()]
+                    cmd_name = line_simple_cmd[0][1]
+                    for line in f.get_line():
+                        if '%' in line:
+                            line = line.split('%', 1)[0] + '%\n'
+                            if line.strip() == '%':
+                                continue
+                        if r'\newcommand{' not in line and r'\renewcommand{' not in line:
+                            for var_name, var_val in var_mapping.iteritems():
+                                if var_name not in complex_cmds:
+                                    line = line.replace(var_name, var_val)
+                        output_file.write(line)
+                        tmp_cmd += [line.strip()]
+                        braces_counter = defaultdict(int, Counter(line))
+                        braces += braces_counter['{'] - braces_counter['}']
+                        #print('-'*80)
+                        #print('line:', line)
+                        #print('open braces:', braces)
+
+                        if braces <= 0:
+                            break
+                    if any(map(lambda x: r'\includegraphics' in x, tmp_cmd)):
+                        tmp_cmd = '\n'.join(tmp_cmd[1:-1])
+                        #print('new cmd:', cmd_name)
+                        #print(tmp_cmd)
+                        complex_cmds[cmd_name] = tmp_cmd
+            elif len(line_simple_cmd) > 0:
+                cmd = "\\" + line_simple_cmd[0][0]
+                #print('\tcmd', cmd)
+                #print(complex_cmds.keys())
+                if cmd in complex_cmds:
+                    vars = tex_utils.get_vars(line)
+                    cmd = complex_cmds[cmd]
+                    #print('ORIG CMD:', cmd)
+                    #print('vars:', vars)
+                    for idx, var in enumerate(vars):
+                        cmd = cmd.replace('#' + str(idx + 1), var)
+                    #print('WRITE cmd:', cmd)
+                    #print('-'*80)
+                    for cmd_line in cmd.split('\n'):
+                        for orig_plot_fn in tex_utils.graphics_cmd_match.findall(cmd_line):
+                            orig_fn, dest_fn = replace_figure(orig_plot_fn, output_folder, file_mapping)
+                            if orig_fn is not None:
+                                cmd_line = cmd_line.replace(orig_plot_fn, dest_fn)
+                        output_file.write(cmd_line + '\n')
+                        #print(cmd_line)
+                    #print('-'*80)
                 else:
                     output_file.write(line)
-            elif not remove_comments:
+            else:
                 output_file.write(line)
+        elif not remove_comments:
+            output_file.write(line)
 
 
 def main():
@@ -126,8 +202,10 @@ def main():
     else:
         os.makedirs(output_folder)
     file_mapping = dict()
+    var_mapping = dict()
+    complex_cmds = dict()
     with open(output_filename, 'w') as outfile:
-        add_content_of_file(paper_filename, outfile, output_folder, file_mapping, remove_comments=remove_comments)
+        add_content_of_file(paper_filename, outfile, output_folder, file_mapping, var_mapping, complex_cmds, remove_comments=remove_comments)
     for fn in os.listdir(os.path.abspath(os.path.dirname(paper_filename))):
         if fn.endswith(('.sty', '.cls')):
             print('copy', fn, 'to', output_folder)
--- tex_utils.py
+++ tex_utils.py
@@ -3,7 +3,26 @@ import re
 simple_cmd_match = re.compile(r'\\([^\\]+?)\{(.*?)\}')
 graphics_cmd_match = re.compile(r'\\includegraphics\[.*?\]?\{(.*?)\}')
 begin_cmd_match = re.compile(r'\\begin{([^}]+?)}(?:(?:\[([^\]]+?)\])|.*)')
+newcmd_match = re.compile(r'\\.+?\{(.*?)\}\{(.*)\}')
+# newcmd_match_with_var = re.compile(r'\\[^\\]+?\{(.*?)\}\{(.*?)\}')
+vars_match = re.compile(r'\{(.+?)\}')
 
+
+def get_vars(line):
+    res = list()
+    open_braces = 0
+    one_var = ''
+    for char in line.strip():
+        if char == '}':
+            open_braces -= 1
+        if open_braces > 0:
+            one_var += char
+        elif open_braces == 0 and one_var:
+            res.append(one_var)
+            one_var = ''
+        if char == '{':
+            open_braces += 1
+    return res
 
 class FileIter:
     def __init__(self, filename):
